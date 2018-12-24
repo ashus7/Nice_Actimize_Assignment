1fe55b61c8c94c1d60cb51206211900dac5f7ab7
ashus7 <ashurulz7@gmail.com>
2018-12-24 23:54:43 +0530

    Remove target

--- target/classes/DDL.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS 'am'.'book';
-CREATE TABLE  'am'.'book' (
-  'id' int(10) unsigned NOT NULL auto_increment,
-  'title' varchar(45) NOT NULL,
-  'author' varchar(45) NOT NULL,
-  PRIMARY KEY  ('id')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
